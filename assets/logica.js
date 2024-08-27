3fcbf4e1f354edf03f63844faac63895e2ffe833
SandraGonzalezPavez <s-agonzalez@hotmail.com>
2024-08-27 19:45:10 -0400

    Primer commit

---
+++ assets/logica.js
@@ -0,0 +1,12 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Aquí puedes añadir funcionalidad para enviar el formulario, 
+    // como usar fetch para enviar los datos a un servidor.
+
+    alert('Formulario enviado!');
+});
+
+function proyectos(){
+    let proyect1=document.getElementById
+}
